8cd61e9504cac39957f8ac412cb59d9dcea64989
Jesse Shawl <jesse@jshawl.com>
2013-03-21 20:12:40 -0400

    organized css

--- js/custom.js
+++ js/custom.js
@@ -62,15 +62,6 @@ overlay_gallery: false, social_tools: false
 });
 
 
-/* Isotype */
-
-// cache container
-var $container = $('#portfolio');
-// initialize isotope
-$container.isotope({
-  // options...
-});
-
 // filter items when filter link is clicked
 $('#filters a').click(function(){
   var selector = $(this).attr('data-filter');
--- js/main.js
+++ js/main.js
@@ -2,3 +2,5 @@
 $(function(){
 $('.table-sorter').tablesorter();
 });
+
+$(".chzn-select").chosen(); $(".chzn-select-deselect").chosen({allow_single_deselect:true});
